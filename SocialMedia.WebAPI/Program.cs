db15280e4a169f82990d4e5535c787acce4de350
Griffin Brown <browgrif@gmail.com>
2022-04-19 18:45:35 -0400

    Dependency injections

--- SocialMedia.WebAPI/Program.cs
+++ SocialMedia.WebAPI/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add Connection string and DbContext Setup
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddControllers();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
