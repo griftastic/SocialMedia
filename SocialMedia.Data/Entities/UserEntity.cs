70810d78acb4b8179dfaff88eca0e22216f8e791
Tim Huser <sidratgames@protonmail.com>
2022-04-19 21:17:52 -0400

    migrate test

--- SocialMedia.Data/Entities/UserEntity.cs
+++ SocialMedia.Data/Entities/UserEntity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,14 +9,14 @@ namespace SocialMedia.Data.Entities
     public class UserEntity
     {
         [Key]
-       public Int Id { get; set;} 
+       public int Id { get; set;} 
        [Required]
        public string Email {get; set;}
        [Required]
        public string Username {get; set;}
        [Required]
        public string Password {get; set;}
-       
+
        public string FirstName {get; set;}
        public string LastName {get; set;}
        [Required]
