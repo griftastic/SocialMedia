7c816a8496cb148bc79683c4e2d0f5f39a41efbe
Tim Huser <sidratgames@protonmail.com>
2022-04-20 20:05:53 -0400

    passHash2

--- SocialMedia.Data/Entities/UserEntity.cs
+++ SocialMedia.Data/Entities/UserEntity.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Microsoft.AspNetCore.Identity;
+
 
 namespace SocialMedia.Data.Entities
 {
@@ -14,12 +16,17 @@ namespace SocialMedia.Data.Entities
        public string Email {get; set;}
        [Required]
        public string Username {get; set;}
-       [Required]
-       public string Password {get; set;}
-       
        public string FirstName {get; set;}
        public string LastName {get; set;}
        [Required]
        public DateTime DateCreated {get; set;}
     }
+    var entity = new UserEntity
+    {
+        Email = model.Email,
+        Username = model.Username,git
+        DateCreated = DateTime.Now
+    };
+    var passwordHasher = new PasswordHasher<UserEntity.();
+    entity.Password = passwordHasher.HashPassword(entity, model.Password);
 }
