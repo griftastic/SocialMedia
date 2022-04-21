2c3c468c66aa83e79ea54570b6c5843b86b07933
jonesarn <59841177+jonesarn@users.noreply.github.com>
2022-04-21 19:11:25 -0400

    last PostEntity stuff

--- SocialMedia.Data/ApplicationDbContext.cs
+++ SocialMedia.Data/ApplicationDbContext.cs
@@ -14,6 +14,6 @@ namespace SocialMedia.Data
         }
         
         public DbSet<UserEntity> Users { get; set; }
-        public DbSet<Post> Post { get; set; }
+        public DbSet<PostEntity> Post { get; set; }
     }
 }
